b653699d9b589bbc04e5c87e51f05751047bdfbb
Feliciano Long <noahzark@vip.qq.com>
2017-07-18 15:58:40 -0500

    move unnecessary functions out of core script

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,15 @@
 var bot_endpoint = 'https://robot-service.centaurstech.com/api/chat'
 
+function show_question(ask) {
+    console.log('user ask: ' + ask)
+}
+
+function show_answer(reply) {
+    console.log('robot answer: ' + reply)
+}
+
 function ask_question(msg, callback) {
-    show_msg(msg)
+    show_question(msg)
 
     var fd = new FormData()
     fd.append('appkey', appkey)
@@ -21,12 +29,7 @@ function ask_question(msg, callback) {
         contentType: false,
         success: function(data, status) {
             if (data.msg != "") {
-                reply = data.msg.replaceAll('\n', '<br />')
-                $('<li class="qw-message">\
-						  <div class="qw-avator"></div>\
-						  <div class="qw-message-content">' + reply + '</div>\
-				  </li>').appendTo(".chat-list");
-                $(".chat-list").scrollTop($(".chat-list")[0].scrollHeight);
+                show_answer(data.msg)
 
                 if (typeof callback == "function") {
                     callback()
@@ -40,36 +43,3 @@ function ask_question(msg, callback) {
         },
     })
 }
-
-function show_msg(msg) {
-    $('<li class="me-message">\
-				<span class="me-message-content">' + msg + '</span>\
-			  </li>').appendTo(".chat-list");
-
-    $(".chat-list").scrollTop($(".chat-list")[0].scrollHeight);
-}
-
-$(".chat-input").keydown(function(e) {
-    if (e.keyCode == 13) {
-        var question = $(".chat-input").val();
-        $(".chat-input").val("");
-
-
-        $('<li class="me-message">\
-        <span class="me-message-content">' + question + '</span>\
-        </li>').appendTo(".chat-list");
-
-        $(".chat-list").scrollTop($(".chat-list")[0].scrollHeight);
-
-        ask_question(question)
-    }
-});
-
-$(document).ready(function() {
-    var search = location.search
-    if (search) {
-        appkey = search.substring(search.lastIndexOf("=") + 1)
-        $('#title').html('<h1>知识库考核 ' + appkey + '</h1>')
-    }
-    ask_question('HELLO')
-})
